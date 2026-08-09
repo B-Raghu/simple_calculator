62659847da310d9212406597177ef9b7d0dd9565
Raghu <rr7548418@gmail.com>
2026-08-09 22:36:55 +0530

    Create calculator

---
+++ Calculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        double num1=sc.nextDouble();
+        System.out.println("Enter the second number:");
+        double num2=sc.nextDouble();
+        System.out.println("Enter Operator:");
+        char operator=sc.next().charAt(0);
+        switch (operator) {
+            case '+':
+                System.out.println(num1+num2);
+                break;
+            case '-':
+                System.out.println(num1-num2);
+                break;
+            case '*':
+                System.out.println(num1*num2);
+                break;
+            case '/':
+                if (num2==0){
+                    System.out.println("Cannot be divided by zero");
+                }else{
+                    System.out.println(num1/num2);
+                }
+                break;
+            default:
+                System.out.println("invalid Operator");
+
+       
+
+        }
+        sc.close();
+        
+        
+    }
+    
+}
