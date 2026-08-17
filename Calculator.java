ad14ca50863fdbf843466368d490b4cdeeb26026
B Raghavendra <rr7548418@gmail.com>
2026-08-17 18:54:22 +0530

    Add remote fetch practice

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,7 @@ public class Calculator {
         Scanner sc=new Scanner(System.in);
         System.out.println("Welcome from main brach!");
         System.out.println("Welcome from conflict branch!");
+        System.out.println("Remote fetch practice");
         System.out.println("Cherry-pick practice!");
         System.out.println("Enter the first number:");
         double num1=sc.nextDouble();
